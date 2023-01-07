fbcb33957427dd47a328197491237003595f034b
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-07 18:25:55 -0300

    feat: create a global style to components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import styled from 'styled-components';
-import { Wrapper } from './style'
+import styled from "styled-components";
 
 function App() {
-
     const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-`;
+        font-size: 1.5em;
+        text-align: center;
+        color: palevioletred;
+    `;
 
-    return (
-        <Wrapper>
-            <Title>Hello World!</Title>
-        </Wrapper>
-    );
+    return <Title>Hello World!</Title>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./createGlobalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GlobalStyle>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </GlobalStyle>
 );
 
 reportWebVitals();

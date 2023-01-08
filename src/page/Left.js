a8a3fbb069057c6f9fa3416d1e21260c969eafb7
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-08 02:31:39 -0300

    feat: create a left component
    Define position and inserting a dinamic background with
    a diferent behavior from figma prototype.

---
+++ src/page/Left.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import elements from "../assets/img/3D_elements/3d_elements.png";
+
+const Left = styled.div`
+    height: 100vh;
+    width: 50vw;
+    background: #E6E7F0;
+    background-image: url(${elements});
+    background-size: cover;
+    background-repeat: no-repeat;
+`;
+
+export default Left;

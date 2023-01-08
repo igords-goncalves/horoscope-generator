634a52a869d04ba1d15736b091c0bfa2521e269f
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-08 02:29:34 -0300

    feat: create a page component and import components

---
+++ src/page/Main.jsx
@@ -0,0 +1,16 @@
+import Left from "./Left";
+import Right from "./Right";
+import Wrapper from "./Wrapper ";
+
+export const Main = () => {
+    return (
+        <Wrapper>
+            <Left>
+                {/* ... */}
+            </Left>
+            <Right>
+                {/* ... */}
+            </Right>
+        </Wrapper>
+    );
+};

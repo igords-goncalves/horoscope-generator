14102146e9ecd9ddc11a9f815a1c54b6db448ea5
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-08 02:25:02 -0300

    fix: fix bug on GlobalStyle reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import GlobalStyle from "./createGlobalStyle.js";
+
 import { Fragment } from "react";
+import { Main } from "./page/Main.jsx";
 
 function App() {
     return (
         <Fragment>
             <GlobalStyle />
+            <Main />
         </Fragment>
     );
 }

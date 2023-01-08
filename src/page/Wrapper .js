b38160deefc3838d16f7655072271be38e263ba6
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-08 02:28:12 -0300

    feat: create an Wrapper component to envolve all

---
+++ src/page/Wrapper .js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export default Wrapper;

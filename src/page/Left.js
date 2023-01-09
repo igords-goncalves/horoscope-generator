cc5a6ea1f4b09840e6c4dd0b63ef783f8bea1c18
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-09 11:11:52 -0300

    style: apply style in components from letf side

--- src/page/Left.js
+++ src/page/Left.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 import elements from "../assets/img/3D_elements/3d_elements.png";
 
 const Left = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; 
+
     height: 100vh;
     width: 50vw;
     background: #E6E7F0;

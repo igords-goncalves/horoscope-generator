85b8122153a1ee6d928a589646670290c621d459
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-09 09:54:52 -0300

    feat: create, export, import a component title

---
+++ src/components/title/index.jsx
@@ -0,0 +1,9 @@
+const Title = (props) => {
+    return (
+        <div>
+            <h1>Horoscope Generator</h1>
+        </div>
+    )
+}
+
+export default Title;
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,4 +1,5 @@
 import { Image } from "../components/image/Image";
+import Title from "../components/title";
 import Left from "./Left";
 import Right from "./Right";
 import Wrapper from "./Wrapper ";
@@ -8,6 +9,7 @@ export const Main = () => {
         <Wrapper>
             <Left>
                 <Image />
+                <Title />
             </Left>
             <Right>
                 {/* ... */}

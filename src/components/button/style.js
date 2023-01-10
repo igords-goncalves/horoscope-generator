1eaea65187394e7af1491aec833bb7ed349fae1f
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-10 17:40:58 -0300

    style: apply style on button component

---
+++ src/components/button/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    button {
+        width: 294px;
+        height: 80px;
+
+        font-size: 2.188em;
+        font-weight: bold;
+        color: #fff;
+
+        background: #01c9b1;
+
+        border-radius: 50px;
+        border: none;
+    }
+
+    a {
+        text-decoration: none;
+        font-size: 1em;
+        color: #6a6a6a;
+    }
+`;
+export default ButtonWrapper;

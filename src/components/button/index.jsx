f7bebb64563084ec017ac62e956ad36d49ae83a8
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-10 17:39:26 -0300

    feat: crete buttonand link to remember age

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,7 +1,14 @@
+import ButtonWrapper from "./style";
+
 const Button = (props) => {
     return (
-        <button onClick={props.getResult}>Generate</button>
-    )
-}
+        <ButtonWrapper>
+            <button onClick={props.getResult}>Generate</button>
+            <a href="https://www.calculator.net/age-calculator.html">
+                forgot your age?
+            </a>
+        </ButtonWrapper>
+    );
+};
 
 export default Button;

d9cd628c5bdbbaf8f74a5722e11446f93dd77a86
Igor Gonçalves <igordossantos38@gmail.com>
2023-12-31 15:20:58 -0300

    Add mockedData.ts with sign models

---
+++ data/mockedData.ts
@@ -0,0 +1,102 @@
+import SignModel from "@/models/Sign.model";
+
+const signs: SignModel[] = [
+  new SignModel(
+    101,
+    "Aries",
+    "Fiery leaders with bold initiative.",
+    "https://imgur.com/gallery/oWeAMW2",
+    321,
+    419
+  ),
+  new SignModel(
+    102,
+    "Taurus",
+    "Grounded and reliable, like the Bull.",
+    "https://imgur.com/gallery/oWeAMW2",
+    420,
+    520
+  ),
+  new SignModel(
+    103,
+    "Gemini",
+    "Adaptable and curious, the Twins of the zodiac.",
+    "https://imgur.com/gallery/oWeAMW2",
+    521,
+    621
+  ),
+  new SignModel(
+    104,
+    "Cancer",
+    "Nurturing and intuitive, like the Crab.",
+    "https://imgur.com/gallery/oWeAMW2",
+    622,
+    722
+  ),
+  new SignModel(
+    105,
+    "Leo",
+    "Bold and charismatic, the Lion of the zodiac.",
+    "https://imgur.com/gallery/oWeAMW2",
+    723,
+    822
+  ),
+  new SignModel(
+    106,
+    "Virgo",
+    "Detail-oriented and practical, the Virgin.",
+    "https://imgur.com/gallery/oWeAMW2",
+    823,
+    922
+  ),
+  new SignModel(
+    107,
+    "Libra",
+    "Balanced and harmonious, the Scales.",
+    "https://imgur.com/gallery/oWeAMW2",
+    923,
+    1023
+  ),
+  new SignModel(
+    108,
+    "Scorpius",
+    "Passionate and determined, the Scorpion.",
+    "https://imgur.com/gallery/oWeAMW2",
+    1024,
+    1121
+  ),
+  new SignModel(
+    109,
+    "Sagittarius",
+    "Adventurous and optimistic, the Archer.",
+    "https://imgur.com/gallery/oWeAMW2",
+    1122,
+    1221
+  ),
+  new SignModel(
+    110,
+    "Capricornus",
+    "Ambitious and disciplined, the Goat.",
+    "https://imgur.com/gallery/oWeAMW2",
+    1222,
+    2119
+  ),
+  new SignModel(
+    111,
+    "Aquarius",
+    "Innovative and humanitarian, the Water Bearer.",
+    "https://imgur.com/gallery/oWeAMW2",
+    2120,
+    2218
+  ),
+  new SignModel(
+    112,
+    "Pisces",
+    "Empathetic and imaginative, the Fish.",
+    "https://imgur.com/gallery/oWeAMW2",
+    2219,
+    2320
+  ),
+];
+
+export default signs;

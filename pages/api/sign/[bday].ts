938b597cea81e2aaa9d77fd5f3271060c97a9d44
Igor Gonçalves <igordossantos38@gmail.com>
2023-12-31 15:21:17 -0300

    Add sign API endpoint for retrieving sign based on birthday

---
+++ pages/api/sign/[bday].ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import signs from "@/data/mockedData";
+import SignModel from "@/models/Sign.model";
+
+type Data = {
+  // FIXME: Argument of type signs missing
+  signs: SignModel[]
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const bday = Number(req.query.bday)
+
+  const signMonthInterval = signs.filter(sign => {
+    const interval = 
+       bday >= sign.initialDate && bday <= sign.finalDate 
+    return interval
+  })
+
+  try {
+    if(signMonthInterval.length === 1) {
+      const sign = signMonthInterval[0]
+      res.status(200).json(sign.objectParse());
+    } else {
+      res.status(204).end()
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}

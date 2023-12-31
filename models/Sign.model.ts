b1284cd92b69a3d4bd0ffb3792a31f627e7962f8
Igor Gonçalves <igordossantos38@gmail.com>
2023-12-31 15:21:07 -0300

    Add SignModel class to models directory

---
+++ models/Sign.model.ts
@@ -0,0 +1,56 @@
+export default class SignModel {
+  #id: number;
+  #title: string;
+  #profile: string;
+  #image: string;
+  #initalDate: number;
+  #finalDate: number;
+
+  constructor(
+    id: number,
+    title: string,
+    profile: string,
+    image: string,
+    initialDate: number,
+    finalDate: number
+  ) {
+    this.#id = id;
+    this.#title = title;
+    this.#profile = profile;
+    this.#image = image;
+    this.#initalDate = initialDate;
+    this.#finalDate = finalDate;
+  }
+
+  get id(): number {
+    return this.#id;
+  }
+
+  get title(): string {
+    return this.#title;
+  }
+
+  get profile(): string {
+    return this.#profile;
+  }
+
+  get image(): string {
+    return this.#image;
+  }
+
+  get initialDate(): number {
+    return this.#initalDate;
+  }
+
+  get finalDate(): number {
+    return this.#finalDate;
+  }
+
+  objectParse() {
+    return {
+      title: this.title,
+      profile: this.profile,
+      image: this.image,
+    };
+  }
+}

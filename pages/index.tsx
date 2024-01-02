8d79874219ee8dd85565539b4cf7799c2e716c7a
Igor Gonçalves <igordossantos38@gmail.com>
2024-01-01 23:27:14 -0300

    Add NextUIProvider to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Card from "@/components/card";
+import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Inter } from "next/font/google";
 
@@ -6,10 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex bg-hero min-h-screen w-full items-center justify-center ${inter.className}`}
-    >
-      <Card />
-    </main>
+    <NextUIProvider>
+      <main
+        className={`flex bg-hero min-h-screen w-full items-center justify-center ${inter.className}`}
+      >
+        <Card />
+      </main>
+    </NextUIProvider>
   );
 }

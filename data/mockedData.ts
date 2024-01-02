556272baa2cc71f68fb6a46d818d5ffc81801c35
Igor Gonçalves <igordossantos38@gmail.com>
2024-01-01 21:14:32 -0300

    Update zodiac sign images

--- data/mockedData.ts
+++ data/mockedData.ts
@@ -5,7 +5,7 @@ const signs: SignModel[] = [
     101,
     "Aries",
     "Fiery leaders with bold initiative.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/927aa0ce4d9adb38c98bb1cd693a0256/raw/91df7fb5acc8ee10571438fde0ce0e3df29baea3/aries.svg",
     321,
     419
   ),
@@ -13,7 +13,7 @@ const signs: SignModel[] = [
     102,
     "Taurus",
     "Grounded and reliable, like the Bull.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/64de665ecb93860e6e6fa8263ef6c5e2/raw/e2f869510fb58ac9b0783f962df37bdd9ffb7af6/taurus.svg",
     420,
     520
   ),
@@ -21,7 +21,7 @@ const signs: SignModel[] = [
     103,
     "Gemini",
     "Adaptable and curious, the Twins of the zodiac.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githubusercontent.com/igords-goncalves/3dc78495e21855d96de4f06aaf3922d8/raw/6d558121bce8aa94db49d363b90874164afac6b5/gemini.svg",
     521,
     621
   ),
@@ -29,7 +29,7 @@ const signs: SignModel[] = [
     104,
     "Cancer",
     "Nurturing and intuitive, like the Crab.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/dc7a611cacb8f15781036a9a975af272/raw/32ce5965b17734e1d5c5f639f90d38f868bd7c09/cancer.svg",
     622,
     722
   ),
@@ -37,7 +37,7 @@ const signs: SignModel[] = [
     105,
     "Leo",
     "Bold and charismatic, the Lion of the zodiac.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/ca5a6faa39455dd41e4071c92288b110/raw/bc28c50488d9f39a9c2b7694e4ec22c56c393729/leo.svg",
     723,
     822
   ),
@@ -45,7 +45,7 @@ const signs: SignModel[] = [
     106,
     "Virgo",
     "Detail-oriented and practical, the Virgin.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/81a4415b0c7a017a995ad2468ffbddf1/raw/97e97dd57ac35b5c1c1ad960ce5aaaee451fee58/virgo.svg",
     823,
     922
   ),
@@ -53,7 +53,7 @@ const signs: SignModel[] = [
     107,
     "Libra",
     "Balanced and harmonious, the Scales.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/00c774ca0bbd2fad6e44cc250c327127/raw/0ae4957dea3cd258689aa7c539a7d0a50cde6f4e/libra.svg",
     923,
     1023
   ),
@@ -61,7 +61,7 @@ const signs: SignModel[] = [
     108,
     "Scorpius",
     "Passionate and determined, the Scorpion.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/4fcec121efbbf4ed7bd35b8e0b2ae3a6/raw/8da6bfbfcd2e47407ac34188d5266af3f80ca2fb/scorpio.svg",
     1024,
     1121
   ),
@@ -69,33 +69,34 @@ const signs: SignModel[] = [
     109,
     "Sagittarius",
     "Adventurous and optimistic, the Archer.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/8149adcd8324ac774a14ab761a078d79/raw/f93499ebe48ff79f13a2be802fbbbd721162fd2a/sargitarius.svg",
     1122,
     1221
   ),
   new SignModel(
     110,
+    //TIP: Problema com passagem do ano adaptação
     "Capricornus",
     "Ambitious and disciplined, the Goat.",
-    "https://imgur.com/gallery/oWeAMW2",
+    "https://gist.githack.com/igords-goncalves/0dedc2921059e3384dd9fa006f56c710/raw/397f8f563ea26f4f0b537e7b341ca7a4ec6051f8/capricornius.svg",
     1222,
-    2119
+    10119
   ),
   new SignModel(
     111,
     "Aquarius",
     "Innovative and humanitarian, the Water Bearer.",
-    "https://imgur.com/gallery/oWeAMW2",
-    2120,
-    2218
+    "https://gist.githack.com/igords-goncalves/70da3b23fa13aa6ab7fe364183e8ed58/raw/0ec5f3742d8d3359069eb7e007bda4d164afb6f5/aquarius.svg",
+    120,
+    218
   ),
   new SignModel(
     112,
     "Pisces",
     "Empathetic and imaginative, the Fish.",
-    "https://imgur.com/gallery/oWeAMW2",
-    20240116,
-    20240130
+    "https://gist.githubusercontent.com/igords-goncalves/21ca9741fba4e2e528c845787d2c6a7c/raw/8a2bd9ee83a21e86d5dff7d6f3c5e5efbe7eb35a/pisces.svg",
+    219,
+    320,
   ),
 ];
 

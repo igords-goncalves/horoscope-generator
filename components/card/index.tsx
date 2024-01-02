2df4ada748f61cfeee24c22d5a1e0fce6dcf2ff6
Igor Gonçalves <igordossantos38@gmail.com>
2024-01-02 00:13:24 -0300

    Update card component styling add responsivity

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -66,7 +66,7 @@ export default function Card() {
   }
 
   return (
-    <div className="card flex flex-col items-center bg-card h-screen w-full max-w-[470px] max-h-[544px] rounded-3xl px-16 pt-8 gap-4">
+    <div className="mr-5 ml-5 card flex flex-col items-center bg-card h-screen w-full max-w-[470px] max-h-[544px] rounded-3xl px-5 sm:px-16 pt-8 gap-4">
       <div className="image flex items-center justify-center w-full max-w-[150px] max-h-[150px] h-full">
         {date ? renderImage() : <Image src={vercel} alt="Vercel"></Image>}
       </div>

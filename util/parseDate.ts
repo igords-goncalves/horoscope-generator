bd9a3ba01f280981af975e9f19506b312b0df774
Igor Gonçalves <igordossantos38@gmail.com>
2024-01-02 00:22:37 -0300

    Fix variable name in parseDate function

--- util/parseDate.ts
+++ util/parseDate.ts
@@ -7,8 +7,8 @@ type ParseDateProps = {
 
 function parseDate({bday, setDate}: ParseDateProps) {
   if(bday) {
-    const AAAA_MM_DD = bday.split("-");
-    const MM_DD = `${AAAA_MM_DD[1]}${AAAA_MM_DD[2]}`
+    const YYYY_MM_DD = bday.split("-");
+    const MM_DD = `${YYYY_MM_DD[1]}${YYYY_MM_DD[2]}`
     let date = parseInt(MM_DD.split("").join(""))
     if (date <= 119) date += 10000;
     return setDate(date);

c65ed02f35169e7538a200c3ab8a4fac0947982e
Igor Gonçalves <igordossantos38@gmail.com>
2024-01-01 21:14:05 -0300

    Refactor parseDate function to handle date format correctly

--- util/parseDate.ts
+++ util/parseDate.ts
@@ -7,7 +7,10 @@ type ParseDateProps = {
 
 function parseDate({bday, setDate}: ParseDateProps) {
   if(bday) {
-    const date = parseInt( bday.split("-").join(""));
+    const AAAA_MM_DD = bday.split("-");
+    const MM_DD = `${AAAA_MM_DD[1]}${AAAA_MM_DD[2]}`
+    let date = parseInt(MM_DD.split("").join(""))
+    if (date <= 119) date += 10000;
     return setDate(date);
   }
   return

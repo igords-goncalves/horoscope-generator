bb7ce9a469258eddea6a3caf82f73cfa002d2dad
Igor Gonçalves <igordossantos38@gmail.com>
2024-03-07 15:04:31 -0300

    feat: add hotjar script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,27 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
+      <Script
+        id="monitor-signs"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3896391,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+        }}
+      />
     </NextUIProvider>
   );
 }

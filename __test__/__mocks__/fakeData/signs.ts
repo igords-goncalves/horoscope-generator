cb540bf3ec090f87b4e95ff09d6021f639a3e641
Igor Gonçalves <igordossantos38@gmail.com>
2025-11-09 22:13:56 -0300

    feat: adiciona dados fictícios para signos e refatora componentes de imagem e título

---
+++ __test__/__mocks__/fakeData/signs.ts
@@ -0,0 +1,31 @@
+import SignModel from "@/models/Sign.model";
+
+ const signs: SignModel[] = [
+  new SignModel(
+    101,
+    "Aries",
+    "Fiery leaders with bold initiative.",
+    "https://gist.githack.com/igords-goncalves/927aa0ce4d9adb38c98bb1cd693a0256/raw/91df7fb5acc8ee10571438fde0ce0e3df29baea3/aries.svg",
+    321,
+    419
+  ),
+  new SignModel(
+    102,
+    "Taurus",
+    "Grounded and reliable, like the Bull.",
+    "https://gist.githack.com/igords-goncalves/64de665ecb93860e6e6fa8263ef6c5e2/raw/e2f869510fb58ac9b0783f962df37bdd9ffb7af6/taurus.svg",
+    420,
+    520
+  ),
+  new SignModel(
+    103,
+    "Gemini",
+    "Adaptable and curious, the Twins of the zodiac.",
+    "https://gist.githubusercontent.com/igords-goncalves/3dc78495e21855d96de4f06aaf3922d8/raw/6d558121bce8aa94db49d363b90874164afac6b5/gemini.svg",
+    521,
+    621
+  ),
+]
+
+
+export default signs;
--- hooks/useIsLoadingData.ts
+++ hooks/useIsLoadingData.ts
@@ -4,10 +4,7 @@ import { useContext } from "react"
 export const useIsLoadingData = () => {
   const {isLoading, setIsLoading} = useContext(IsLoadingContext)
 
-  // Nesse custom hook. poderiamos adicionar mais lógica, 
-  // como por exemplo, um console.log para debugar o estado de isLoading
-
-  if(isLoading) console.log('isLoading is true')
+  if(isLoading) console.info('isLoading is true')
 
   return {isLoading, setIsLoading}
 }
